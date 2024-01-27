2be4050427e51923d7334f857c136255485e97d3
Aye10032 <aye10032@gmail.com>
2024-01-27 17:05:53 +0800

    文件上传界面

---
+++ pages/1_File_Upload.py
@@ -0,0 +1,26 @@
+from io import StringIO
+
+import streamlit as st
+
+st.set_page_config(page_title="微藻文献大模型知识库", page_icon="📖", layout='centered')
+st.title('添加文献')
+
+with st.sidebar:
+    st.title('使用说明')
+    st.subheader('PDF')
+    st.markdown(
+        """由于学术论文的PDF中排版和图表的干扰，预处理较为复杂，建议尽量先在本地处理为markdown文件后再上传    
+        1. 将PDF文件重命名为`PMxxxx.pdf`的格式          
+        2. 确保grobid已经在运行     
+        3. 上传PDF文件      
+        4. 等待处理完成     
+        """
+    )
+
+uploaded_files = st.file_uploader('选择PDF或markdown文件', type=['md'], accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    string_data = stringio.read()
+    st.markdown(string_data)
