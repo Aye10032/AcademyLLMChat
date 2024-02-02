e5cdaa53cb8282988e325890f32d6d05704393b3
Aye10032 <aye10032@gmail.com>
2024-02-02 16:30:20 +0800

    添加进度条、处理异常文章

--- InitDatabase.py
+++ InitDatabase.py
@@ -4,6 +4,8 @@ from langchain.text_splitter import MarkdownHeaderTextSplitter, RecursiveCharact
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.vectorstores.milvus import Milvus
 from loguru import logger
+from tqdm import tqdm
+
 from Config import config
 from utils.TimeUtil import timer
 
@@ -13,7 +15,7 @@ logger.add('log/runtime.log')
 @timer
 def load_md(base_path):
     md_splitter = MarkdownHeaderTextSplitter(
-        headers_to_split_on=[('#', 'Title'), ('##', 'SubTitle'), ('###', 'Title3')]
+        headers_to_split_on=[('#', 'Title'), ('##', 'Section'), ('###', 'Subtitle'), ('####', 'Subtitle')]
     )
     r_splitter = RecursiveCharacterTextSplitter(
         chunk_size=450,
@@ -24,15 +26,20 @@ def load_md(base_path):
     logger.info('start building vector database...')
     milvus_cfg = config.milvus_config
 
+    model = milvus_cfg.get_model()
+    collection = milvus_cfg.get_collection()['collection_name']
+
+    logger.info(f'load collection [{collection}], using model {model}')
+
     embedding = HuggingFaceBgeEmbeddings(
-        model_name=milvus_cfg.get_model(),
+        model_name=model,
         model_kwargs={'device': 'cuda'},
         encode_kwargs={'normalize_embeddings': True}
     )
 
     vector_db = Milvus(
         embedding,
-        collection_name=milvus_cfg.get_collection()['collection_name'],
+        collection_name=collection,
         connection_args={
             'host': milvus_cfg.MILVUS_HOST,
             'port': milvus_cfg.MILVUS_PORT
@@ -45,11 +52,10 @@ def load_md(base_path):
     logger.info('start loading file...')
 
     for root, dirs, files in os.walk(base_path):
-        for file in files:
+        for file in tqdm(files, total=len(files)):
             file_path = os.path.join(root, file)
             file_year = int(os.path.basename(root))
             doi = file.replace('@', '/').replace('.md', '')
-            logger.info(f'loading <{file}> ({file_year}) {file_path}...')
 
             with open(file_path, 'r', encoding='utf-8') as f:
                 md_text = f.read()
@@ -59,9 +65,15 @@ def load_md(base_path):
                 doc.metadata['year'] = file_year
             md_docs = r_splitter.split_documents(head_split_docs)
 
-            vector_db.add_documents(md_docs)
+            # logger.info(f'loading <{file}> ({file_year})...')
+            try:
+                vector_db.add_documents(md_docs)
+            except Exception as e:
+                logger.error(f'loading <{file}> ({file_year}) fail')
+                logger.error(e)
     logger.info(f'done')
 
 
 if __name__ == '__main__':
-    load_md(config.MD_OUTPUT)
+    config.set_collection(1)
+    load_md(config.MD_PATH)
