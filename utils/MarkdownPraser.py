81f5d0f3fe7b154f787d35a1987a39efb7a7d72c
Aye10032 <aye10032@gmail.com>
2024-02-21 15:53:22 +0800

    修改splitter逻辑

--- utils/MarkdownPraser.py
+++ utils/MarkdownPraser.py
@@ -47,13 +47,13 @@ def split_markdown_text(md_text: str, **kwargs):
         doi = kwargs.get('doi')
         author = kwargs.get('author')
     else:
-        yaml_text = head_split_docs[0].page_content.replace('---', '')
+        yaml_text = head_split_docs.pop(0).page_content.replace('---', '')
         data = yaml.load(yaml_text, Loader=yaml.FullLoader)
         year = data['year']
         doi = data['doi']
         author = data['author']
 
-    for doc in head_split_docs[1:]:
+    for doc in head_split_docs:
         doc.metadata['doi'] = doi
         doc.metadata['year'] = int(year)
         doc.metadata['author'] = author
