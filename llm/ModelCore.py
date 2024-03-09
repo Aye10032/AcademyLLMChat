664dc56d35ab35d939aa74a74ae1b65491253c6b
Aye10032 <aye10032@gmail.com>
2024-03-09 15:55:22 +0800

    更换中英文embedding模型

--- llm/ModelCore.py
+++ llm/ModelCore.py
@@ -1,17 +1,17 @@
 import httpx
 import streamlit as st
 from langchain_anthropic import ChatAnthropic
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings, HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 
 from Config import config
 
 
 @st.cache_resource(show_spinner=f'Loading {config.milvus_config.EN_MODEL}...')
-def load_embedding_en() -> HuggingFaceBgeEmbeddings:
+def load_embedding_en() -> HuggingFaceEmbeddings:
     model = config.milvus_config.EN_MODEL
 
-    embedding = HuggingFaceBgeEmbeddings(
+    embedding = HuggingFaceEmbeddings(
         model_name=model,
         model_kwargs={'device': 'cuda'},
         encode_kwargs={'normalize_embeddings': True}
