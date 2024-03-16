0a4ab4cbf0b0adc484cef727e5803e47a705b3c0
Aye10032 <aye10032@gmail.com>
2024-03-16 15:57:04 +0800

    添加注释

--- utils/FileUtil.py
+++ utils/FileUtil.py
@@ -14,9 +14,9 @@ class Section:
 
 def format_filename(filename):
     """
-    Format filename to remove invalid characters
-    :param filename:
-    :return:
+    格式化文件名，移除无效字符
+    :param filename: 需要格式化的文件名
+    :return: 格式化后的文件名
     """
     valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
     filename = ''.join(c for c in filename if c in valid_chars)
@@ -56,24 +56,29 @@ def is_en(text: str):
 
 def save_to_md(sections: list[Section], output_path, append: bool = False, **kwargs):
     """
-    Save sections to markdown file
-    :param sections: markdown结构化段落
-    :param output_path: markdown文件输出路径
-    :param append: 是否追加写入
-    :return:
+    将章节列表保存为Markdown格式的文件。
+
+    :param sections: 包含章节内容的列表，每个章节都由Section类型表示。
+    :param output_path: 输出Markdown文件的路径。
+    :param append: 是否追加模式，默认为False，即覆盖原有文件；如果为True，则追加到文件末尾。
+    :param kwargs: 可选参数，用于在文件开头写入额外的信息，如引用(ref)、作者(author)、年份(year)和DOI(doi)。
+    :return: 无返回值。
     """
+    # 根据append参数决定文件打开模式
     if append:
         open_type = 'a'
     else:
         open_type = 'w'
 
     with open(output_path, open_type, encoding='utf-8') as f:
+        # 如果非追加模式，写入额外信息（如引用、作者、年份和DOI）
         if not append:
             ref: bool = kwargs.get('ref')
             year: str = kwargs.get('year')
             author: str = kwargs.get('author')
             doi: str = kwargs.get('doi')
             f.write(f'---\nref: {ref}\t\nauthor: {author}\t\nyear: {year}\t\ndoi: {doi}\t\n---\n\n')
+        # 遍历章节列表，根据章节级别写入相应格式的文本
         for sec in sections:
             text = sec.text
             level = sec.level
@@ -90,6 +95,16 @@ def save_to_md(sections: list[Section], output_path, append: bool = False, **kwa
 
 
 def section_to_documents(sections: list[Section], author: str, year: int, doi: str) -> list[Document]:
+    """
+    将章节列表转换为文档列表。
+
+    :param sections: 一个Section类型的列表，表示文档的各个章节。
+    :param author: 文档的作者。
+    :param year: 文档发表的年份。
+    :param doi: 文档的数字对象标识符。
+
+    :return: 一个Document类型的列表，每个元素代表一个章节的内容及其元数据。
+    """
     __Title = ''
     __Section = ''
     docs: list[Document] = []
@@ -101,6 +116,7 @@ def section_to_documents(sections: list[Section], author: str, year: int, doi: s
             case 2:
                 __Section = section.text
             case 0:
+
                 docs.append(Document(
                     page_content=section.text,
                     metadata={
