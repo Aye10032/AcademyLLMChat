45fab7bb3aabf85558a36f70aafd1a49fe98f4f7
Aye10032 <aye10032@gmail.com>
2024-03-16 15:27:52 +0800

    添加注释

--- utils/DecoratorUtil.py
+++ utils/DecoratorUtil.py
@@ -6,11 +6,21 @@ from loguru import logger
 
 
 def timer(func: Callable) -> Callable:
+    """
+    一个装饰器，用于给函数计时。
+
+    参数:
+    - func: Callable，要被计时的函数。
+
+    返回值:
+    - Callable，一个封装了原函数的计时器函数。
+    """
+
     @wraps(func)
     def wrapper(*args, **kwargs):
-        start = time.time()
+        start = time.perf_counter()
         result = func(*args, **kwargs)
-        end = time.time()
+        end = time.perf_counter()
         logger.debug(f'{func.__name__} Time cost: {end - start}s')
         return result
 
@@ -19,20 +29,25 @@ def timer(func: Callable) -> Callable:
 
 def retry(retries: int = 3, delay: float = 1) -> Callable:
     """
-    Attempt to call a function, if it fails, try again with a specified delay.
+    为函数提供重试逻辑的装饰器。
 
-    :param retries: The max amount of retries you want for the function call
-    :param delay: The delay (in seconds) between each function retry
-    :return:
+    参数:
+    retries (int): 最大重试次数，默认为3。
+    delay (float): 两次重试之间的延迟时间（秒），默认为1。
+
+    返回:
+    Callable: 被装饰的函数。
+
+    异常:
+    ValueError: 如果retries小于1或delay小于等于0，则抛出此异常。
     """
     if retries < 1 or delay <= 0:
-        raise ValueError('Are you high, mate?')
+        raise ValueError('Wrong param')
 
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args, **kwargs) -> Any:
             for i in range(1, retries + 1):
-
                 try:
                     return func(*args, **kwargs)
                 except Exception as e:
@@ -41,7 +56,7 @@ def retry(retries: int = 3, delay: float = 1) -> Callable:
                         logger.error(f'"{func.__name__}()" failed after {retries} retries.')
                         break
                     else:
-                        logger.warning(f'Error: {repr(e)} -> Retrying...')
+                        logger.debug(f'Error: {repr(e)} -> Retrying...')
                         time.sleep(delay)
 
         return wrapper
