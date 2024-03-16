1dc26338f687785be381eb94d46e08712e9900cf
Aye10032 <aye10032@gmail.com>
2024-03-16 16:21:53 +0800

    添加注释

--- utils/PMCUtil.py
+++ utils/PMCUtil.py
@@ -9,10 +9,9 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup, Tag, NavigableString
 from loguru import logger
-from pandas import DataFrame
 from requests import sessions
 
-from Config import config
+from Config import Config
 from utils.FileUtil import Section, replace_multiple_spaces
 from utils.DecoratorUtil import timer, retry
 
@@ -35,6 +34,14 @@ id_length = RefIdType.UNFIXED
 @timer
 @retry(delay=random.uniform(2.0, 5.0))
 def get_pmc_id(term: str, file_name: str = 'pmlist.csv') -> None:
+    """
+    根据提供的搜索术语获取PMC（PubMed Central）文章ID，并将结果保存到CSV文件中。
+
+    :param term: 搜索术语，用于在PMC数据库中进行搜索
+    :param file_name: 保存结果的CSV文件名，默认为'pmlist.csv'
+    :return: 无返回值
+    """
+
     url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term={term}'
 
     headers = {
@@ -55,28 +62,46 @@ def get_pmc_id(term: str, file_name: str = 'pmlist.csv') -> None:
 
 
 @retry(delay=random.uniform(2.0, 5.0))
-def download_paper_data(pmc_id: str) -> Tuple[int, dict]:
+def download_paper_data(pmc_id: str, config: Config = None) -> Tuple[int, dict]:
+    """
+    下载指定PMC文章的XML数据，并将其存储到本地文件系统中。
+
+    :param pmc_id: 文章的PMC标识符。
+    :param config: 包含配置信息的对象，如API密钥和代理设置。如果未提供，则使用默认配置。
+    :return: 一个元组，包含HTTP响应状态码和一个字典，字典包含文章的年份、DOI和输出路径。
+    """
+    # 检查是否提供了配置对象，未提供则使用默认配置
+    if config is None:
+        config = Config()
+
+    # 构建下载文章数据的URL
     url = (f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc&id={pmc_id}'
            f'&retmode=xml&api_key={config.pubmed_config.API_KEY}')
 
+    # 设置HTTP请求头，伪装为Chrome浏览器发送请求
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     }
 
+    # 使用Session对象进行HTTP请求，支持使用代理
     with sessions.Session() as session:
         if config.pubmed_config.USE_PROXY:
+            # 如果配置了使用代理，则从配置中获取代理地址
             proxies = {
                 'http': config.get_proxy(),
                 'https': config.get_proxy()
             }
             response = session.request("GET", url, headers=headers, proxies=proxies, timeout=10)
         else:
+            # 否则直接发送请求
             response = session.request("GET", url, headers=headers, timeout=10)
 
+    # 检查HTTP响应状态码，若非200，则抛出异常
     if response.status_code == 200:
-
+        # 使用BeautifulSoup解析XML响应内容
         soup = BeautifulSoup(response.text, 'xml')
 
+        # 提取文章的DOI和发表年份
         doi = soup.find('article-id', {'pub-id-type': 'doi'}).text \
             if soup.find('article-id', {'pub-id-type': 'doi'}) \
             else None
@@ -85,56 +110,79 @@ def download_paper_data(pmc_id: str) -> Tuple[int, dict]:
             if soup.find('pub-date') \
             else None
 
+        # 根据DOI和年份构建本地存储路径
         xml_path = os.path.join(config.get_xml_path(), year, doi.replace('/', '@') + '.xml') if doi else None
 
+        # 如果路径存在，创建目录并将XML数据写入文件
         if xml_path:
             os.makedirs(os.path.dirname(xml_path), exist_ok=True)
 
             with open(xml_path, 'w', encoding='utf-8') as f:
                 f.write(response.text)
 
+        # 返回HTTP状态码和相关元数据
         return response.status_code, {
             'year': year,
             'doi': doi,
             'output_path': xml_path
         }
     else:
+        # 如果请求失败，抛出异常
         raise Exception('下载请求失败')
 
 
 def parse_paper_data(xml_text: str, year: str, doi: str) -> dict:
+    """
+    解析论文数据从给定的XML文本中。
+
+    :param xml_text: 论文的XML格式文本。
+    :param year: 论文的出版年份。
+    :param doi: 论文的数字对象标识符（DOI）。
+    :return: 包含论文标题、作者、年份、DOI、章节和规范化状态的字典。
+
+    此函数从XML文本中提取论文的相关信息，包括标题、作者、摘要、章节以及引用信息，并将这些信息组织成一个字典返回。
+    """
+
+    # 使用BeautifulSoup解析XML文本
     soup = BeautifulSoup(xml_text, 'xml')
 
     try:
+        # 尝试获取纸质版本的ISSN号，如果不存在则获取电子版本的ISSN号
         issn_block = soup.find('front').find('journal-meta').find('issn', {'pub-type': 'ppub'})
         if not issn_block:
             issn_block = soup.find('front').find('journal-meta').find('issn', {'pub-type': 'epub'})
 
         issn = issn_block.text if issn_block else None
-    finally:
-        issn = None
+    except Exception as e:
+        logger.warning(e)
+        issn = None  # 如果无法获取ISSN号，则最终设置为None
 
+    # 检查ISSN号是否需要修正，并设置相应的标识
     if issn in fix_journal:
         global id_length
         id_length = RefIdType.FIXED
 
+    # 初始化论文章节列表
     sections: list[Section] = []
 
+    # 提取并处理论文标题
     title = soup.find('article-title').text.replace('\n', ' ') \
         if soup.find('article-title') \
         else None
     title = replace_multiple_spaces(title)
-
     sections.append(Section(title, 1))
 
+    # 提取论文作者信息
     author_block = soup.find('contrib-group').find('name')
     author = __extract_author_name(author_block) if author_block else None
 
+    # 提取论文摘要和正文部分
     abs_block = soup.find('abstract')
     main_sections = soup.select_one('body')
 
     norm = True
 
+    # 尝试提取论文引用信息
     ref_block = soup.select_one('back')
     if ref_block:
         pass
@@ -149,10 +197,12 @@ def parse_paper_data(xml_text: str, year: str, doi: str) -> dict:
             'norm': False
         }
 
+    # 如果存在摘要，将其添加为一个章节，并处理摘要内容及引用信息
     if abs_block:
         sections.append(Section('Abstract', 2))
         sections = __solve_section(abs_block, sections, 2, ref_block)
     else:
+        # 如果没有摘要，记录警告并返回部分解析的结果
         logger.warning(f'{doi} has no Abstract')
         return {
             'title': title,
@@ -163,9 +213,11 @@ def parse_paper_data(xml_text: str, year: str, doi: str) -> dict:
             'norm': False
         }
 
+    # 处理正文部分的章节信息
     if main_sections:
         sections = __solve_section(main_sections, sections, 1, ref_block)
 
+    # 返回解析后的论文信息
     return {
         'title': title,
         'author': author,
@@ -177,11 +229,21 @@ def parse_paper_data(xml_text: str, year: str, doi: str) -> dict:
 
 
 def __extract_author_name(xml_block: BeautifulSoup | NavigableString | None) -> str:
+    """
+    从XML块中提取作者的姓名。
+
+    :param xml_block: 包含作者信息的BeautifulSoup对象或NavigableString对象，或为None。
+    :return: 格式化后的作者姓名，格式为"姓, 名首字母."。
+    """
+    # 提取作者的姓氏
     surname = xml_block.find('surname').text
+    # 尝试提取作者的名字，如果不存在则默认为空字符串
     given_names = xml_block.find('given-names').text if xml_block.find('given-names') else ''
 
+    # 将名字拆分并提取首字母，形成缩写
     initials = ' '.join([name[0] + '.' for name in given_names.split()])
 
+    # 返回格式化后的姓名
     return f'{surname}, {initials}'
 
 
@@ -226,51 +288,47 @@ def __solve_section(
 
 def __solve_ref(ref_soup: BeautifulSoup, ref_list: list[Tag]) -> str:
     """
-    解析参考文献列表，并根据其类型提取并转换为DOI列表。
+    处理参考文献引用，根据不同的引用类型生成对应的DOI列表。
 
-    参数:
-    - ref_soup: BeautifulSoup对象，包含参考文献的HTML解析树。
-    - ref_list: Tag列表，每一个Tag代表一个参考文献的引用。
-
-    返回值:
-    - str: 通过逗号分隔的DOI字符串列表。
+    :param ref_soup: BeautifulSoup对象，包含参考文献的HTML文档片段
+    :param ref_list: Tag列表，每一个Tag代表一个参考文献引用
+    :return: 字符串，包含所有参考文献的DOI，通过逗号分隔
     """
     global id_length
     rid_list = []
     for ref in ref_list:
-        logger.debug(ref)  # 记录调试信息：当前处理的参考文献引用
-        content = ref.text  # 获取参考文献的文本内容
+        content = ref.text
         if is_single_reference(content) == RefType.SINGLE:
-            # 单个参考文献处理
+            # 处理单个参考文献引用
             rid_list.append(ref['rid'])
         elif is_single_reference(content) == RefType.MULTI:
             # 处理范围引用的参考文献
             if id_length == RefIdType.UNFIXED:
                 _r_numbers = parse_range_string(content)  # 解析范围字符串
                 _rid = remove_last_digit(ref['rid'])  # 移除最后一个数字
-                logger.debug(_rid)  # 记录调试信息：处理后的引用ID
                 rid_list.extend([f'{_rid}{i}' for i in _r_numbers])  # 生成范围内的ID列表
             else:
                 _r_numbers = parse_range_string(content)  # 解析范围字符串
                 _rid, digit = remove_digit_and_return(ref['rid'])  # 移除数字并返回位数
-                logger.debug(_rid)  # 记录调试信息：处理后的引用ID
                 rid_list.extend([f'{_rid}{str(i).zfill(digit)}' for i in _r_numbers])  # 生成对齐位数的范围ID列表
         else:
-            logger.error('unknown type')  # 记录错误：未知的引用类型
+            logger.error('unknown type')  # 记录未知的引用类型错误
 
     rid_list = sorted(list(set(rid_list)))  # 去重并排序引用ID列表
-    logger.debug(rid_list)  # 记录调试信息：处理后的引用ID列表
+    logger.debug(rid_list)  # 记录处理后的引用ID列表
 
     doi_list = []
     for ref_id in rid_list:
+        # 查找对应的参考文献块
         ref_block = ref_soup.find(['ref', 'element-citation'], {'id': ref_id})
         if ref_block is None:
             ref_block = ref_soup.find(attrs={'id': ref_id})
 
+        # 尝试获取DOI
         doi_block = ref_block.find('pub-id', {'pub-id-type': 'doi'})
         doi_list.append(doi_block.text) if doi_block else None
 
-    return ','.join([x for x in doi_list])
+    return ','.join([x for x in doi_list])  # 返回DOI列表，通过逗号分隔
 
 
 def is_single_reference(text: str) -> RefType:
