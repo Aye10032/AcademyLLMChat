028a1d80365f9b2a62078f0fb93d2a1dc7592a10
Aye10032 <aye10032@gmail.com>
2024-05-06 00:40:15 +0800

    修复milvus查询错误

--- pages/FileUpload.py
+++ pages/FileUpload.py
@@ -64,8 +64,8 @@ def __check_exist(ref_list: DataFrame) -> DataFrame:
     ref_list['exist'] = False
     with MilvusConnection(**milvus_cfg.get_conn_args()) as conn:
         for index, row in ref_list.iterrows():
-            _num = conn.client.query(milvus_cfg.get_collection().collection_name, filter=f'doi == "{row.doi}"')
-            if len(_num) != 0:
+            _num = len(conn.client.query(milvus_cfg.get_collection().collection_name, filter=f'doi == "{row.doi}"'))
+            if _num != 0:
                 ref_list.at[index, 'exist'] = True
 
     return ref_list
@@ -227,7 +227,7 @@ def markdown_tab():
 
                 doi = doc[0].metadata.get('doi')
                 with MilvusConnection(**milvus_cfg.get_conn_args()) as conn:
-                    _num = conn.client.query(milvus_cfg.get_collection().collection_name, filter=f'doi == "{doi}"')
+                    _num = len(conn.client.query(milvus_cfg.get_collection().collection_name, filter=f'doi == "{doi}"'))
                 if _num > 0:
                     st.error(f'文章 {doi} 已存在，跳过')
                     continue
