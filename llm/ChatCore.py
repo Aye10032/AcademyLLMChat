2a549228f94121e4e865a0b79b15f2997bee3dce
Aye10032 <aye10032@gmail.com>
2024-09-06 08:43:02 +0800

    add system prompt

--- llm/ChatCore.py
+++ llm/ChatCore.py
@@ -44,14 +44,13 @@ def write_paper(_chat_history: ChatMessageHistory | StreamlitChatMessageHistory,
             SystemMessage(
                 content='你是一个科研工作者，正在进行一些科研文献和基金项目申请的工作，因此你的写作风格需要符合科研文献的一般风格。'
                         '当需要查询信息时，优先使用向量数据库进行搜索，仅当使用者要求联网搜索时才调用工具进行联网查询。'),
-            MessagesPlaceholder(variable_name='chat_history'),
-            ('human', '{input}'),
+            MessagesPlaceholder(variable_name='chat_history')
         ]
     )
 
     retrieve_tool = VecstoreSearchTool(target_collection='temp1')
     web_tool = WebSearchTool()
-    tools = [retrieve_tool]
+    tools = [retrieve_tool, web_tool]
 
     llm = load_gpt4o_mini()
     llm_with_tools = llm.bind_tools(tools)
@@ -62,14 +61,17 @@ def write_paper(_chat_history: ChatMessageHistory | StreamlitChatMessageHistory,
     messages.append(HumanMessage(question))
 
     with st.spinner('正在分析您的需求...'):
-        ai_msg = llm_with_tools.invoke(messages)
+        ai_msg = history_chain.invoke({'chat_history': messages})
 
     messages.append(ai_msg)
     for tool_call in ai_msg.tool_calls:
-        selected_tool = {"search_from_vecstore": retrieve_tool}[tool_call["name"].lower()]
+        selected_tool = {
+            'search_from_vecstore': retrieve_tool,
+            'search_from_web': web_tool,
+        }[tool_call["name"].lower()]
         tool_output = selected_tool.invoke(tool_call["args"])
         messages.append(ToolMessage(tool_output, tool_call_id=tool_call["id"]))
 
-    result = llm_with_tools.stream(messages)
+    result = history_chain.stream({'chat_history': messages})
 
     return result
