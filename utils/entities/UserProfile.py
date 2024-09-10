75fa949c7e574ee6f4de0a4e8035039e3e48dbba
Aye10032 <tc_aye@outlook.com>
2024-09-10 10:45:57 +0800

    add user data

---
+++ utils/entities/UserProfile.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass, asdict
+from enum import IntEnum
+from typing import Any
+
+
+class UserGroup(IntEnum):
+    VISITOR = 0
+    WRITER = 1
+    FILE_ADMIN = 2
+    ADMIN = 3
+
+
+@dataclass
+class User:
+    name: str
+    password: str
+    user_group: int
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]):
+        return cls(**data)
+
+
+@dataclass
+class Project:
+    name: str
+    owner: User
+
+
+def main() -> None:
+    user = User.from_dict({
+        "name": "aye",
+        "password": "114514",
+        "user_group": UserGroup.ADMIN
+    })
+
+
+if __name__ == '__main__':
+    main()
